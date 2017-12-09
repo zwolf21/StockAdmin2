ced7c01089326dbd915b55e8632e8b417cfb271f
zwolf21 <pbr112@naver.com>
2017-12-09 18:19:06 +0900

    제품앱에 Market, Product 모델 생성

--- StockAdmin2/product/models.py
+++ StockAdmin2/product/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Market(models.Model):
+    name = models.CharField('도매상명', max_length=50, unique=True)
+    tel = models.CharField('전화', max_length=50, blank=True, null=True)
+    fax = models.CharField('팩스', max_length=50, blank=True, null=True)
+    email = models.EmailField('메일', blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = '도매상'
+        verbose_name_plural = '도매상'
+
+    def __str__(self):
+        return self.name
+
+
+
+STD_UNITs = 'TAB', 'CAP', 'VIAL', 'AMP', 'TUBE', 'SYR', 'PEN', 'BTL', 'BAG', '포',
+ETC_CLASS = '일반', '항암제', '마약', '향정', '수액', '영양수액', '인슐린주사', '백신', '조영제', '처치약품', '소모품', '직송'
+
+class Product(models.Model):
+    name = models.CharField('제품명', max_length=50, unique=True)
+    code = models.CharField('제품코드', max_length=50, unique=True)
+    company = models.CharField('판매사', max_length=50, blank=True)
+    std_unit = models.CharField('제품규격', max_length=10, choices=zip(STD_UNITs, STD_UNITs))
+    pkg_amount = models.IntegerField('포장수량', default=1)
+    etc_class = models.CharField('기타구분', max_length=10, choices=zip(ETC_CLASS, ETC_CLASS), default='일반')
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = '제품'
+        verbose_name_plural = '제품'
+        ordering = '-created',
+
+    def __str__(self):
+        return self.name
+
+   
+
+
+
+
+
