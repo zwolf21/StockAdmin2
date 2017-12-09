42fbcf3f220be1bb9cd2be37e6cc85b99b4727ad
zwolf21 <pbr112@naver.com>
2017-12-09 18:27:34 +0900

    product 앱 admin 적용

--- StockAdmin2/product/admin.py
+++ StockAdmin2/product/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Market
+
+
+@admin.register(Market)
+class MarkeAdmin(admin.ModelAdmin):
+    list_display = 'name', 'tel', 'fax', 'email',
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = 'code', 'name', 'company', 'std_unit', 'pkg_amount',
+    list_filter = 'std_unit',
+    search_fields = 'name', 'company',
+    list_editable = 'name', 'company', 'std_unit', 'pkg_amount',
