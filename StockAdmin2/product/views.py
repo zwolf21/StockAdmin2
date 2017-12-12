85a5999bb4d8e32834cd9e03053b696fffed91d8
zwolf21 <pbr112@naver.com>
2017-12-12 23:50:16 +0900

    product 리스트에 필터 구현

--- StockAdmin2/product/views.py
+++ StockAdmin2/product/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from django.views.generic import *
 
 from .models import Market, Product, BuyInfo
-
+from core.filter import QueryFilter
 
 class ProductListView(ListView):
     model = Product
+
+    def get_queryset(self):
+        qs = super(ProductListView, self).get_queryset()
+        qf = QueryFilter(self.request, qs)
+        qs = qf.filter_by_search()
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        qf = QueryFilter(self.request)
+        context['search_filter_form'] = qf.get_search_filter_form()
+        return context
     
